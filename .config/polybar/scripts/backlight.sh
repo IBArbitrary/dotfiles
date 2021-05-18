ebb8dbff0ab85374269455f30476e2914ff06e72
IBArbitrary <k.rajeshkumar.1411@gmail.com>
2021-05-18 14:34:49 +0530

    some changes

--- .config/polybar/scripts/backlight.sh
+++ .config/polybar/scripts/backlight.sh
@@ -1,3 +1,6 @@
-#!/bin/bash
-
-xbacklight -getf | cut -d. -f1 | awk '{printf "%3d", $1}'
+#!/usr/bin/env python3
+import os
+#xbacklight -getf | cut -d. -f1 | awk '{printf "sike %3s", $1}'
+cmd = "xbacklight -getf | cut -d. -f1"
+#brightness = os.system(cmd)
+os.system("echo a {}".format(os.system(cmd)))
