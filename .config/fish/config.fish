082516ea80ef919cb1007d4538e247ab442df1aa
IBArbitrary <k.rajeshkumar.1411@gmail.com>
2021-06-06 15:54:35 +0530

    added arch ascii art

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -147,7 +147,8 @@ abbr -a sk "screenkey --bg-color '#282828' --font-color '#a89984' -s small --vis
 abbr -a mp "ncmpcpp"
 abbr -a et "emacsclient -t -a 'vim'"
 abbr -a e 'emacsclient --create-frame --alternate-editor="" &'
-abbr -a pgp 'pass git push origin master'
+abbr -a pgpush 'pass git push origin master'
+abbr -a pgpull 'pass git pull origin master'
 abbr -a mci 'sudo make clean install'
 abbr -a ls 'exa'
 abbr -a lsa 'exa -a'
