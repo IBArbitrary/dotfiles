ed275244f1b29a73558fa4b3b8e09f71185b7348
IBArbitrary <k.rajeshkumar.1411@gmail.com>
2021-08-17 18:32:48 +0530

    many little changes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -72,7 +72,7 @@ alias xx 'exec sh -c "startx; sudo /usr/bin/prime-switch"'
 # doom-emacs
 alias doom "~/packages/doom-emacs/bin/doom"
 alias orca xxiivv-orca
-abbr -a mine 'padsp java -jar ~/packages/tlauncher/TLauncher-2.75.jar'
+abbr -a mine 'prime-run padsp java -jar ~/packages/tlauncher/TLauncher-2.75.jar'
 
 function pacs
     pacman -Slq | fzf --prompt 'pacman> ' \
