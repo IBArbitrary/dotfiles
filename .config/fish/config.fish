d8064573831c1093c1594680f541f648d4992bbf
IBArbitrary <k.rajeshkumar.1411@gmail.com>
2022-03-06 10:09:36 +0530

    [fish] restructured config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,27 +1,30 @@
-# color customisations
-# set -U fish_color_command brgreen
-# set -U fish_color_cwd brgreen
-# set -U fish_color_cwd_root brred
-# set -U fish_color_error brred
-# set -U fish_color_escape bryellow
-# set -U fish_color_operator brblue
-# set -U fish_color_quote bryellow
-# set -U fish_color_redirection brcyan
-# set -U fish_color_status brred
+################################################################################
+# ███████╗██╗███████╗██╗  ██╗
+# ██╔════╝██║██╔════╝██║  ██║
+# █████╗  ██║███████╗███████║
+# ██╔══╝  ██║╚════██║██╔══██║
+# ██║     ██║███████║██║  ██║
+# ╚═╝     ╚═╝╚══════╝╚═╝  ╚═╝
+################################################################################
+# o┳━┓┳━┓┳━┓┳━┓o┏┓┓┳━┓┳━┓┳━┓┓ ┳
+# ┃┃━┃┃━┫┃┳┛┃━┃┃ ┃ ┃┳┛┃━┫┃┳┛┗┏┛
+# ┇┇━┛┛ ┇┇┗┛┇━┛┇ ┇ ┇┗┛┛ ┇┇┗┛ ┇
+################################################################################
 
+# display random splash art
 function splash
     set -l splash (find ~/.config/fish/scripts/color-scripts/ -type f | shuf -n 1)
     $splash
     set -e splash
 end
 
+# greeting function for fish
 function fish_greeting
     set me (whoami)
     echo -e "\e[2;37mhello there, \e[1m$me\e[0m\e[2;37m.\e[0m"
 end
 
-
-# reloading fish config
+# reload fishconfig
 function reload
     clear
     source ~/.config/fish/config.fish
@@ -29,81 +32,42 @@ function reload
     neofetch
 end
 
-abbr -a r reload
-zoxide init fish | source
-
-#========== FZF ================================= 
-
-export FZF_DEFAULT_COMMAND="fd -t f"
-export FZF_DEFAULT_OPTS='--color=bg+:#3c3836,bg:#32302f,spinner:#fb4934,hl:#928374,fg:#ebdbb2,header:#928374,info:#8ec07c,pointer:#fb4934,marker:#fb4934,fg+:#ebdbb2,prompt:#fb4934,hl+:#fb4934 --layout=reverse --border=rounded --prompt="# " --pointer=">" --marker="+"'
-export FZF_ALT_C_OPTS="--height=50% --black --preview 'tree -C {} | head -200' --header='Choose directory to change to.'"
-export FZF_ALT_C_COMMAND="fd -t d -H"
-export FZF_CTRL_T_COMMAND="fd -H"
-export FZF_CTRL_T_OPTS="--height=50% --black --header='Choose file/folder.'"
-export FZF_CTRL_R_OPTS="--height=50% --black --header='Choose command from history.' --preview 'echo {}' --preview-window down:3:hidden:wrap --bind '?:toggle-preview'"
-
-# powershell habit
+# clear snippet, powershell style
 function cls
     command clear
 end
 
-abbr -a c clear
-
-# wallpaper-slideshow
+# start wallpaper carousel
 function wp-ss
     bgchd -dir /media/storage/pictures/wallpapers/ -bcknd feh -intv 10m -rpl
 end
 
-# dotfiles alias for bare git repo
-alias dots '/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-alias la 'ls -ah --group-directories-first'
-alias lA 'ls -Ah --group-directories-first'
-alias ll 'ls -oh'
-alias lla 'ls -oah'
-alias llA 'ls -oAh'
-alias lc 'ls | wc -l'
-alias lca 'la | wc -l'
-alias lcA 'lA | wc -l'
-
-# x-server + prime-switch
-alias xx 'exec sh -c "startx; sudo /usr/bin/prime-switch"'
-
-# doom-emacs
-alias doom "~/packages/doom-emacs/bin/doom"
-
-alias orca xxiivv-orca
-alias b "buku --suggest"
-alias zat zathura
-alias zar zaread
-alias ssh "kitty +kitten ssh"
-
-# identify key id
+# identify keypress name
 function idkey
     xev | awk -F'[ )]+' '/^KeyPress/ { a[NR+2] } NR in a { printf "%-3s %s\n", $5, $8 }'
 end
 
+# identify window class name
 function idwin
     xprop | grep WM_CLASS
 end
 
+# snippet useful for wordle games
 function words
     grep "^[a-z]\{$argv[1]\}\$" /usr/share/dict/words
 end
 
+# mount function
 function mnt
     sudo mount "/dev/$argv[1]" "/media/$argv[2]"
 end
 
+# unmount function
 function umnt
     sudo umount "/media/$argv[1]"
 end
 
-
-abbr -a mntu "mnt sda1 usb"
-abbr -a mnte "mnt sda1 external"
-abbr -a umntu "umnt usb"
-abbr -a umnte "umnt external"
-
+# fzf aided package installer
 function pacs
     pacman -Slq | fzf --prompt 'pacman> ' \
         --header 'Install packages.
@@ -115,6 +79,7 @@ function pacs
     pacman -Qqe >~/.config/pac-kages.txt
 end
 
+# fzf aided package uninstaller
 function pacr
     pacman -Qq | fzf --prompt 'all> ' \
         --header='Remove packages.
@@ -127,33 +92,31 @@ function pacr
     pacman -Qqe >~/.config/pac-kages.txt
 end
 
-export MANPAGER="sh -c 'col -bx | bat -l man -p --paging always'"
-
-# embeds vobsub subtitle into video
+# snippet to embed vobsub sub onto video
 function vobsub-embed
     ffmpeg -i $argv[1] -i $argv[2] -i $argv[3] \
         -map 0:v -map 0:a -c copy -map 1 -c:s:1 dvd_subtitle -metadata:s:s:1 \
         language=eng $argv[4]
 end
 
-# get google drive images viewable link
+# snippet to convert gdrive link to static img link
 function gdrive-img-link
     echo $argv[1] \
         | grep -Po "(?<=drive.google.com/file/d/)(.*)(?=/view?.*)" \
         | sed "s/^/https:\/\/drive.google.com\/uc?export=view\&id=/"
 end
 
-
-# file-opener function. might come in handy.
+# return files using fzf search
 function fzfr
     fzf -m | xargs -d'\n' -r $argv
 end
 
+# negating rm
 function rmn
     find . -type f -not -name "$argv" | xargs rm
 end
 
-# dir comparer
+# compare dirs for backup purposes
 function bu-compdir
     if test -z $argv[3]
         diff -q $argv[1] $argv[2] | grep -Po "(?<=Only in $argv[1]: )(.*)"
@@ -162,50 +125,43 @@ function bu-compdir
     end
 end
 
-
-# checking install history of pacman
-abbr -a pach "expac --timefmt='%Y-%m-%d %T' '%l\t%n' | sort -r | fzf"
-
-# check updates
-abbr -a cu checkupdates
-abbr -a syu "sudo pacman -Syu"
-# quit vim syntax
-abbr -a :q exit
-
+# open multi pdfs as tabbed zathura
 function zt
     tabbed -c -r 2 zathura -e id $argv & disown
 end
 
+# quick cd to config dirs
 function cf
     cd ~/.config
     set config_dir (fd -t d -H | fzf)
     cd $config_dir
 end
 
-export VISUAL='emacsclient -c -a ""'
-export EDITOR='emacsclient -t -a "vim"'
-export TERM='xterm-kitty'
-export YTFZF_ENABLE_FZF_DEFAULT_OPTS=1
-export BAR_VISIBLE=1
-export PATH="$PATH:$HOME/.rvm/bin"
-export SXHKD_SHELL='sh'
-export GOPATH="$HOME/.go"
-export SSH_AUTH_SOCK=(gpgconf --list-dirs agent-ssh-socket)
+# miscellaneous commands
+zoxide init fish | source
+eval 'dircolors ~/.dircolors' >/dev/null
 gpgconf --launch gpg-agent
-# java app shenangians
-export _JAVA_OPTIONS='-Dawt.useSystemAAFontSettings=on'
-export _JAVA_AWT_WM_NONREPARENTING=1
-export AWT_TOOLKIT=MToolKit
 
-abbr -a fc "emacsclient --create-frame --alternate-editor="" ~/.config/fish/config.fish &"
-abbr -a nus "vnstat -d"
-abbr -a g git
+# abbreviations
+abbr -a :q exit
+abbr -a bks 'cd /media/storage/books/'
+abbr -a bum "bu-compdir /media/storage/movies/ /media/external/movies/"
+abbr -a bus "bu-compdir /media/storage/series/ /media/external/series/"
+abbr -a c clear
+abbr -a cu checkupdates
 abbr -a da "dots add"
 abbr -a dau "dots add -u"
 abbr -a dc "dots commit"
 abbr -a dcm "dots commit -m"
+abbr -a di "dict -d gcide"
+abbr -a docs 'cd /media/storage/documents/'
 abbr -a dp "dots push"
 abbr -a ds "dots status"
+abbr -a e 'emacsclient --create-frame --alternate-editor="" &'
+abbr -a et "emacsclient -t -a 'vim'"
+abbr -a fc "emacsclient --create-frame --alternate-editor="" ~/.config/fish/config.fish &"
+abbr -a fm 'pcmanfm & disown'
+abbr -a g git
 abbr -a ga "git add"
 abbr -a gad "git add ."
 abbr -a gau "git add -u"
@@ -215,45 +171,83 @@ abbr -a gp "git push"
 abbr -a gpb "git push both"
 abbr -a grv "git remote -v"
 abbr -a gs "git status"
-abbr -a pi "sudo pip3 install"
-abbr -a pu "sudo pip3 uninstall"
-abbr -a pi2 "sudo pip install"
-abbr -a q exit
-abbr -a st "cd /media/storage/"
-abbr -a pc "sudo pacman -Sc"
-abbr -a yc "yay -Sc"
-abbr -a rd "sudo pacman -Rcns"
-abbr -a di "dict -d gcide"
-abbr -a th "dict -d moby-thesaurus"
-abbr -a sstar "sudo systemctl start"
-abbr -a sstop "sudo systemctl stop"
-abbr -a sstat "sudo systemctl status"
-abbr -a senab "sudo systemctl enable"
-abbr -a sdesb "sudo systemctl disable"
-abbr -a nf neofetch
-abbr -a ytf "youtube-dl -F"
-abbr -a ytd youtube-dl
-abbr -a sk "screenkey --bg-color '#282828' --font-color '#a89984' -s small --vis-shift -t 0.5"
-abbr -a mp ncmpcpp
-abbr -a et "emacsclient -t -a 'vim'"
-abbr -a e 'emacsclient --create-frame --alternate-editor="" &'
-abbr -a pgpush 'pass git push origin master'
-abbr -a pgpull 'pass git pull origin master'
-abbr -a mci 'sudo make clean install'
-abbr -a pg 'pacgraph -b "#282828" -l "#665c54" -t "#b8bb26" -d "#a89984" -p 5 40 -e -s --disable-palette -f (date +%s)'
-abbr -a yt 'ytfzf --subt -tf --preview-side=right'
-abbr -a xo xdg-open
-abbr -a fm 'pcmanfm & disown'
-abbr -a sub 'subliminal download -l en'
-abbr -a ser 'cd /media/storage/series/'
-abbr -a mov 'cd /media/storage/movies/'
-abbr -a proj 'cd /media/storage/projects/'
-abbr -a docs 'cd /media/storage/documents/'
-abbr -a bks 'cd /media/storage/books/'
-abbr -a mans "man -k . | cut -d "-" -f1 | fzf | cut -d " " -f1 | xargs -r man"
 abbr -a hor "cd /media/storage/horizon/"
-abbr -a bum "bu-compdir /media/storage/movies/ /media/external/movies/"
-abbr -a bus "bu-compdir /media/storage/series/ /media/external/series/"
+abbr -a mans "man -k . | cut -d "-" -f1 | fzf | cut -d " " -f1 | xargs -r man"
+abbr -a mci 'sudo make clean install'
+abbr -a mnte "mnt sda1 external"
+abbr -a mntu "mnt sda1 usb"
+abbr -a mov 'cd /media/storage/movies/'
+abbr -a mp ncmpcpp
+abbr -a nf neofetch
+abbr -a nus "vnstat -d"
+abbr -a pach "expac --timefmt='%Y-%m-%d %T' '%l\t%n' | sort -r | fzf"
+abbr -a pc "sudo pacman -Sc"
+abbr -a pg 'pacgraph -b "#282828" -l "#665c54" -t "#b8bb26" -d "#a89984" -p 5 40 -e -s --disable-palette -f (date +%s)'
+abbr -a pgpull 'pass git pull origin master'
+abbr -a pgpush 'pass git push origin master'
+abbr -a pi "sudo pip3 install"
+abbr -a pi2 "sudo pip install"
+abbr -a proj 'cd /media/storage/projects/'
+abbr -a pu "sudo pip3 uninstall"
+abbr -a q exit
+abbr -a r reload
+abbr -a rd "sudo pacman -Rcns"
+abbr -a sdesb "sudo systemctl disable"
+abbr -a senab "sudo systemctl enable"
+abbr -a ser 'cd /media/storage/series/'
+abbr -a sk "screenkey --bg-color '#282828' --font-color '#a89984' -s small --vis-shift -t 0.5"
+abbr -a sstar "sudo systemctl start"
+abbr -a sstat "sudo systemctl status"
+abbr -a sstop "sudo systemctl stop"
+abbr -a st "cd /media/storage/"
+abbr -a sub 'subliminal download -l en'
+abbr -a syu "sudo pacman -Syu"
+abbr -a th "dict -d moby-thesaurus"
+abbr -a umnte "umnt external"
+abbr -a umntu "umnt usb"
+abbr -a xo xdg-open
+abbr -a yc "yay -Sc"
+abbr -a yt 'ytfzf --subt -tf --preview-side=right'
+abbr -a ytd youtube-dl
+abbr -a ytf "youtube-dl -F"
 
+# aliases
+alias b "buku --suggest"
+alias doom "~/packages/doom-emacs/bin/doom"
+alias dots '/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+alias lA 'ls -Ah --group-directories-first'
+alias la 'ls -ah --group-directories-first'
+alias lc 'ls | wc -l'
+alias lcA 'lA | wc -l'
+alias lca 'la | wc -l'
+alias ll 'ls -oh'
+alias llA 'ls -oAh'
+alias lla 'ls -oah'
+alias orca xxiivv-orca
+alias ssh "kitty +kitten ssh"
+alias xx 'exec sh -c "startx; sudo /usr/bin/prime-switch"'
+alias zar zaread
+alias zat zathura
 alias zathurat 'tabbed -c -r 2 zathura -e id'
-eval 'dircolors ~/.dircolors' >/dev/null
+
+# env var exports
+export AWT_TOOLKIT=MToolKit
+export BAR_VISIBLE=1
+export EDITOR='emacsclient -t -a "vim"'
+export FZF_ALT_C_COMMAND="fd -t d -H"
+export FZF_ALT_C_OPTS="--height=50% --black --preview 'tree -C {} | head -200' --header='Choose directory to change to.'"
+export FZF_CTRL_R_OPTS="--height=50% --black --header='Choose command from history.' --preview 'echo {}' --preview-window down:3:hidden:wrap --bind '?:toggle-preview'"
+export FZF_CTRL_T_COMMAND="fd -H"
+export FZF_CTRL_T_OPTS="--height=50% --black --header='Choose file/folder.'"
+export FZF_DEFAULT_COMMAND="fd -t f"
+export FZF_DEFAULT_OPTS='--color=bg+:#3c3836,bg:#32302f,spinner:#fb4934,hl:#928374,fg:#ebdbb2,header:#928374,info:#8ec07c,pointer:#fb4934,marker:#fb4934,fg+:#ebdbb2,prompt:#fb4934,hl+:#fb4934 --layout=reverse --border=rounded --prompt="# " --pointer=">" --marker="+"'
+export GOPATH="$HOME/.go"
+export MANPAGER="sh -c 'col -bx | bat -l man -p --paging always'"
+export PATH="$PATH:$HOME/.rvm/bin"
+export SSH_AUTH_SOCK=(gpgconf --list-dirs agent-ssh-socket)
+export SXHKD_SHELL='sh'
+export TERM='xterm-kitty'
+export VISUAL='emacsclient -c -a ""'
+export YTFZF_ENABLE_FZF_DEFAULT_OPTS=1
+export _JAVA_AWT_WM_NONREPARENTING=1
+export _JAVA_OPTIONS='-Dawt.useSystemAAFontSettings=on'
