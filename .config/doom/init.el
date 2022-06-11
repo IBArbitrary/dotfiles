3f65fa77ae3c709ba02dfdaaa4ca45e60662c8d7
IBArbitrary <k.rajeshkumar.1411@gmail.com>
2022-06-11 16:32:18 +0530

    [doom-emacs] added julia

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -169,7 +169,7 @@
        ;;(java +meghanada) ; the poster child for carpal tunnel syndrome
        (javascript
         +lsp)       ; all(hope(abandon(ye(who(enter(here))))))
-       ;;julia             ; a better, faster MATLAB
+       julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
        (latex
         +cdlatex
